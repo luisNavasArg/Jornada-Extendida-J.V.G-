041df9351464e33caf71e11eae85089c38152165
LuisNavasArg <lpipnavas@gmail.com>
2019-10-25 15:15:17 -0300

    incusión del header

--- js/script.js
+++ js/script.js
@@ -1,19 +1,27 @@
 let espacios = [{
 	nombre:"Teatro", src:"imagenes/teatro.jpg",
-	descripcion:`Este espacio educativo se propone desarrollar la capacidad ex-
-presiva y creativa, así como favorecer las habilidades socioemo-
-cionales fundamentales de los niños, niñas y adolescentes de las
-
-escuelas públicas de la Ciudad de Buenos Aires a través de expe-
-riencias en las que representen situaciones con el propio cuerpo,
-
-expresándose a través del gesto, el movimiento y la voz.
+	descripcion:`Este espacio educativo se propone desarrollar la 
+	capacidad expresiva y creativa, así como favorecer las habilidades 
+	socioemocionales fundamentales de los niños, niñas y adolescentes 
+	de las escuelas públicas de la Ciudad de Buenos Aires a través de 
+	experiencias en las que representen situaciones con el propio cuerpo,
+	expresándose a través del gesto, el movimiento y la voz.
 
 Asimismo, propicia el desarrollo de la alfabetización estético-
 expresiva (en este caso, teatral) a través del ejercicio de la ob-
 servación, la exploración y la consecuente apropiación de las
 
-posibilidades de comunicación personal y grupal.`
+posibilidades de comunicación personal y grupal.`,
+fundamentacion:`El teatro como juego de ficción es una vía que facilita el 
+desarrollo de una imaginación creadora que inventa partiendo de la realidad, 
+y la transforma. El teatro es el juego organizado del‘como si’; el juego de 
+representar otras realidades comprometiendo el propio cuerpo como medio para 
+expresar y comunicar ideas, emociones y sensaciones.<br>El teatro es una actividad 
+que promueve el conocimiento de uno mismo, porque se involucran el movimiento, 
+la acción y la voz, implicando la intervención total del cuerpo, los pensamientos, 
+la acción, el gesto y la palabra. En este espacio educativo se propician el 
+autoconocimiento, la autoestima, el manejo de emociones, el trabajo grupal, 
+la resolución de conflictos y la comunicación entre iguales.`
 },{
 	nombre:"Lúdico Expresivo", src:"imagenes/ludico.jpg",
 	descripcion:`
@@ -24,13 +32,37 @@ competencias tanto motrices y emocionales como comunicativas y
 sociales.
 Se intenta promover en los participantes la construcción de saberes 
 para disfrutar y disponer de su cuerpo, jugar, expresarse y compartir
- con otros.`
+ con otros.`,
+ fundamentacion:`Al hablar de competencias lúdicas, se hace 
+ referencia a la adquisición de una “disponibilidad para jugar”, 
+ de la capacidad de “saber jugar” y de saberes éticos, tácticos, 
+ estratégicos, técnicos, reglamentarios, aplicables tanto en los 
+ juegos y deportes como a la diferentes actividades expresivas y 
+ gimnásticas.<br>Cuando se hace referencia a las competencias 
+ comunicacionales nos referimos a aquellas que nos permiten 
+ expresarnos y compartir con otros, destcando en este punto la 
+ importancia de que los niños, niñas y adolescentes de nuestras 
+ escuelas enriquezcan y amplíen sus posibilidades de expresión 
+ así como de interacción entre ellos: que puedan relacionarse e 
+ interpretar sus mensajes.`
 },{
 	nombre:"Actividades Acuáticas",src:"imagenes/ludico2.jpg",
 	descripcion:`En este espacio se desarrolla la supervivencia en el agua
 	de un modo constructivo y lúdico. También cuenta con una parte 
-	recreativa en donde los alumnos logran desarrollar y mejorar sus habilidades motrices dentro del agua.`
-},{
+	recreativa en donde los alumnos logran desarrollar y mejorar sus habilidades motrices dentro del agua.`,
+	fundamentacion:`Al hablar de competencias lúdicas, se hace 
+	 referencia a la adquisición de una “disponibilidad para jugar”, 
+	 de la capacidad de “saber jugar” y de saberes éticos, tácticos, 
+	 estratégicos, técnicos, reglamentarios, aplicables tanto en los 
+	 juegos y deportes como a la diferentes actividades expresivas y 
+	 gimnásticas.<br>Cuando se hace referencia a las competencias 
+	 comunicacionales nos referimos a aquellas que nos permiten 
+	 expresarnos y compartir con otros, destcando en este punto la 
+	 importancia de que los niños, niñas y adolescentes de nuestras 
+	 escuelas enriquezcan y amplíen sus posibilidades de expresión 
+	 así como de interacción entre ellos: que puedan relacionarse e 
+	 interpretar sus mensajes.`
+	},{
 	nombre:"Investigación en Acción",src:"imagenes/iea2.jpg",
 	descripcion:`La propuesta de este espacio en particular implica 
 	abordar los significados de “ser estudiante”, con el objetivo de 
@@ -38,18 +70,59 @@ para disfrutar y disponer de su cuerpo, jugar, expresarse y compartir
 tales. Se promueven instancias para que sean capaces de conocerse e 
 identificar sus fortalezas y dificultades, con el fin de que puedan 
 construir herramientas de estudio para resolver las diferentes demandas 
-de las asignaturas y futuras.`
+de las asignaturas y futuras.`,
+fundamentacion:`Como se plantea en el Diseño Curricular, el pasaje de 
+la escuela primaria al secundario implica muchos cambios en cuanto a 
+la cultura institucional y académica, da lugar a una
+“ruptura que instala discontinuidades, que son tanto temidas
+como valoradas”.Por ello, creemos fundamental la existencia de 
+un espacio que permita formar a los niños, niñas  adolescentes como 
+estudiantes, con el fin de que adquieran las herramientas de estudio necesarias para sostener su tra-
+yectoria educativa. En concordancia con los lineamientos de Jornada Extendida,
+este espacio se caracteriza por ofrecer a los estudiantes experiencias 
+pedagógicas innovadoras que faciliten el acceso a las habilidades y 
+las competencias necesarias para la construcción del rol de 
+estudiante, que se vincula al eje “Autonomía en el estudio” de la 
+propuesta de Tutoría de la NES, como ya hemos mencionado.`
 },{
 	nombre:"Ajedrez",src:"imagenes/ajedrez.jpg",
 	descripcion:`Este espacio educativo se propone ofrecer a 
 	los estudiantes la posibilidad de aproximarse al ajedrez 
 	como espacio lúdico propicio para el desarrollo de las 
-	capacidades de análisis, selección y elaboración de estrategias.`
+	capacidades de análisis, selección y elaboración de estrategias.`,
+	fundamentacion:`
+
+Actualmente, son muy reconocidas las fortalezas de esta pro-
+puesta para el desarrollo de los niños, niñas y adolescentes; no
+
+solo para los que demuestran tener facilidad para avanzar en
+este juego, sino para el conjunto de los estudiantes.
+Las clases de ajedrez promueven la atención y la concentración,
+que resultan fundamentales para la vida escolar. Asimismo, se
+
+potencia la memoria en relación con el movimiento de las pie-
+zas y reglas básicas y también a partir de la propia experiencia
+
+de lo ocurrido en partidas anteriores.
+Asimismo propicia el desarrollo de la creatividad en tanto los
+estudiantes deben poner en juego todo su potencial creativo
+para generar nuevas alternativas.
+Finalmente, y no menos importante, resulta una práctica que
+
+promueve el desarrollo de la autonomía al requerir que el suje-
+to sea protagonista y se comprometa con su propio aprendiza-
+je desarrollando una confianza creciente en sus posibilidades.`
 },{
 	nombre:"Programación",src:"imagenes/programacion.jpg",
-	descripcion:`Este espacio es para realizar los primeros contactos 
-	de los alumnos con la programación con el pensamiento computacional, innovación, creatividad y el trabajo colaborativo.`
-}];
+	descripcion:`   Este espacio es para realizar los primeros contactos 
+	de los alumnos con la programación con el pensamiento computacional, 
+	innovación, creatividad y el trabajo colaborativo.`,
+fundamentacion:`Es una de las Areas de mucha importancia en las ciencias 
+de la educación. Promueve por medio de programas adecuados para las y los 
+adolescentes de educación secundaria la lógica para aplicarla 
+en la vida cotidiana, la orientación de este espacio ayuda a desarrollar 
+habilidades de abstracción, la cual sirve para simplificar la solución de 
+problemas `}];
 const inicio=()=>{
 	for(let a=0; a < espacios.length; a++){
 		document.getElementById('principal').innerHTML+=`
@@ -57,8 +130,8 @@ const inicio=()=>{
 			    <img src="${espacios[a].src}" class="card-img-top" alt="">
 			    <div class="card-body">
 			      	<h5 class="card-title text-center">${espacios[a].nombre}</h5>
-			      	<p class="card-text">${espacios[a].descripcion}</p>
-			      	<button type="button" onclick="mostrar(${a})" class="btn btn-secondary">Ver Más!</button>
+			      	<p class="card-text text-justify">${espacios[a].descripcion}</p>
+			      	<button type="button" onclick="mostrarMas(${a})" class="btn btn-secondary">Ver Más!</button>
 			    </div>
 		  	</div>
 		`;
@@ -66,10 +139,16 @@ const inicio=()=>{
 };
 inicio();
 
-const mostrarMas=()=>{
+const mostrarMas=(esp)=>{console.log(espacios);
+	let cab=document.querySelectorAll('header');
+	cab[0].style.backgroundImage=`url(${espacios[esp].src})`;
 	document.getElementById('principal').innerHTML=`
 		<div class="col-12">
-			
+			<p class="text-justify">${espacios[esp].fundamentacion}</p>
+			<button type="button" onclick="volver()" class="btn btn-secondary">Volver</button>
 		</div>
-	`;
-}
+			`;
+};
+const volver=()=>{
+	window.location.reload();
+};
